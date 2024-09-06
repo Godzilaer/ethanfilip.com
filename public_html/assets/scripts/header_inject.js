715297647f87ef6e350ccc1f45c8957df86bbcdc
Godzilaer <114835170+Godzilaer@users.noreply.github.com>
2024-09-05 22:29:03 -0400

    good stuff
    js header standardized injected instead of independent with every html file
    
    folder structure changes

---
+++ public_html/assets/scripts/header_inject.js
@@ -0,0 +1,18 @@
+const header = document.body.getElementsByTagName("header")[0];
+const headerHTML = 
+`<div class="header-title-logo-holder">
+    <a href="/">
+        <img src="/assets/images/logo_circle.png" class="header-logo">
+    </a>
+    
+    <h1>The Ethan Web</h1>
+</div>
+
+<nav>
+    <a class="header-nav-button first-a"href="/">Home</a>
+    <a class="header-nav-button" href="/games">Games</a>
+    <a class="header-nav-button" href="/tools">Tools</a>
+    <a class="header-nav-button" href="/current-projects">Current Projects</a>
+</nav>`
+
+header.innerHTML += headerHTML;
