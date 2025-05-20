7031e21648e7b46c826d472811ace5bf6596f2ba
Godzilaer <114835170+Godzilaer@users.noreply.github.com>
2025-05-19 22:51:55 -0400

    update

--- src/data/projects.js
+++ src/data/projects.js
@@ -19,6 +19,6 @@ const project = (name, type, desc, img, externalLink=null) => ({
 export default [
   project('Air Traffic Troubles', projectTypes.game, 'Air traffic control strategy and puzzle game where you must guide planes safely to land.', 'air-traffic-troubles.png'),
   project('Clock', projectTypes.tool, 'Analog clock with an ocean background. Might add timers and alarms.', 'clock.png'),
-  project('Coin Flipper', projectTypes.game, 'A 3D physics-based coin flipping sandbox. Includes a graph to see how probability approaches expected value over time.', 'coin-flipper.png'),
+  //project('Coin Flipper', projectTypes.game, 'A 3D physics-based coin flipping sandbox. Includes a graph to see how probability approaches expected value over time.', 'coin-flipper.png'),
   //project('Coding Club', projectTypes.website, 'The Dover Sherborn High School coding club website.', 'coding_club.png', 'github.com'),
 ]
