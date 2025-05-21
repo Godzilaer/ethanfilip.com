2d8a4859f8c875079db83aa4893be98346d5fa3a
Godzilaer <114835170+Godzilaer@users.noreply.github.com>
2025-05-20 22:44:50 -0400

    forgor to remove test thing

--- src/data/extracurriculars.js
+++ src/data/extracurriculars.js
@@ -43,6 +43,5 @@ export default {
         hobby("Programming", "Programming began as a hobby for me that I found fun and relaxing. While it has continued to be this, it has also expanded into a main interest of mine."),
         hobby("Flight Simulation", "I have been very interested by planes for a long time. The way large hunks of metal can come together so specifically to create a flying being is astonisting to me. I began with the X-Plane flight simulator and spent many hours learning the real life procedures for airliners and flying their routes. Then, I began connecting to a global network called VATSIM. Here, I enhanced the realism by having access to a network of air traffic controllers who followed real life procedures to the T that I would have to speak to and coordinate with to complete a flight. At the start of 2025, I also took an interest for the Air Traffic Control side of things and began training on Vatsim to become a controller in the Boston airspace. In March I became a student ground controller."),
         hobby("Gaming", "I really enjoy playing games with friends. I especially enjoy cooperative games."),
-        hobby("test", "test")
     ]
 }
