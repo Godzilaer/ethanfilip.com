92898fbeb7d4a5a6cf988c92826e55c1bb4a7e27
Ethan F <ethie.alexandru@gmail.com>
2025-05-23 11:05:08 -0400

    small code reorg
    -moved fadeInEffect outside of App function

--- src/App.jsx
+++ src/App.jsx
@@ -29,32 +29,32 @@ export default function App() {
       <Footer />
     </>
   )
+}
 
-  function fadeInEffect() {
-    const elems = document.querySelectorAll('main *');
+function fadeInEffect() {
+  const elems = document.querySelectorAll('main *');
 
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                const element = entry.target;
+  const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+              const element = entry.target;
 
-                element.classList.add('fade-in-running');
-   
-                element.addEventListener("animationend", () => {
-                    element.classList.remove('fade-in-running');
+              element.classList.add('fade-in-running');
+  
+              element.addEventListener("animationend", () => {
+                  element.classList.remove('fade-in-running');
 
-                    element.classList.add('fade-in-end');
-                }, { once: true });
+                  element.classList.add('fade-in-end');
+              }, { once: true });
 
-                observer.unobserve(element);
-            }
-        });
-    }, 
-    //10% of element seen
-    { threshold: 0.1 });
+              observer.unobserve(element);
+          }
+      });
+  }, 
+  //10% of element seen
+  { threshold: 0.1 });
 
-    elems.forEach((element) => {
-        observer.observe(element);
-    });
-  }
+  elems.forEach((element) => {
+      observer.observe(element);
+  });
 }
