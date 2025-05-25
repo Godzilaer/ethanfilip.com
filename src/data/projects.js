961485dfca63a624a692df165d0597b02cb2432e
Ethan F <ethie.alexandru@gmail.com>
2025-05-25 14:29:12 -0400

    added flappy pengoon

--- src/data/projects.js
+++ src/data/projects.js
@@ -20,6 +20,6 @@ export default [
   project('Air Traffic Troubles', projectTypes.game, 'Air traffic control strategy and puzzle game where you must guide planes safely to land.', 'air-traffic-troubles.png'),
   project('Clock', projectTypes.tool, 'Analog clock with an ocean background. Might add timers and alarms.', 'clock.png'),
   project('Coin Flipper', projectTypes.game, 'A 3D physics-based coin flipping sandbox. Includes a graph to see how probability approaches expected value over time.', 'coin-flipper.png'),
-  project('Flappy Pengoon', projectTypes.game, 'Uh oh, our good old bird friend has arrived in the arctic. Help him dodge ice spikes! Note: Penguins can\'t fly :(', 'flappy-pengoon.png'),
+  project('Flappy Pengoon', projectTypes.game, 'Uh oh, our good old bird friend has arrived in the arctic. Help him dodge the ice spikes! Note: Penguins can\'t actually fly :(', 'flappy-pengoon.png'),
   //project('Coding Club', projectTypes.website, 'The Dover Sherborn High School coding club website.', 'coding_club.png', 'github.com'),
 ]
